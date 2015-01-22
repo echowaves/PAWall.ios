61ec529058a61d023f23892433f9099453465774
dmitryame <dmitry@custommode.com>
2015-01-22 11:18:42 -0500

    search improved

--- PAWall/SearchAdsViewController.swift
+++ PAWall/SearchAdsViewController.swift
@@ -11,7 +11,8 @@ import MapKit
 
 class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchDisplayDelegate {
     
-    var adsNearMe:[PFObject] = []
+    var adsNearMe = [PFObject]()
+    var fileteredAdsNearMe = [PFObject]()
     var myLocation:PFGeoPoint = PFGeoPoint()
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -29,11 +30,54 @@ class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableVie
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-//        self.searchBar.becomeFirstResponder()
-        self.searchBar.showsCancelButton = false
+
+        PFGeoPoint.geoPointForCurrentLocationInBackground {
+            (geoPoint: PFGeoPoint!, error: NSError!) -> Void in
+            
+            if error == nil {
+                // do something with the new geoPoint
+                self.myLocation = geoPoint
+                
+                // Create a query for places
+                var query = PFQuery(className:CLASSIFIED_AD.CLASS_NAME)
+                // Interested in locations near user.
+                query.whereKey(CLASSIFIED_AD.LOCATION, nearGeoPoint:self.myLocation)
+                query.whereKey(CLASSIFIED_AD.ACTIVE, equalTo: true)
+                // Limit what could be a lot of points.
+                query.limit = 100
+                // Final list of objects
+                //                self.adsNearMe =
+                
+                query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
+                    if error == nil {
+                        // The find succeeded.
+                        // Do something with the found objects
+                        
+                        
+                        NSLog("Successfully retrieved \(objects.count)")
+                        if objects.count > 0 {
+                            self.adsNearMe = objects as [PFObject]
+                            self.tableView.reloadData()
+                        }
+                        
+                    } else {
+                        // Log details of the failure
+                        NSLog("Error: %@ %@", error, error.userInfo!)
+                        
+                        let alertMessage = UIAlertController(title: "Error", message: "Error retreiving ads, try agin.", preferredStyle: UIAlertControllerStyle.Alert)
+                        let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in})
+                        alertMessage.addAction(ok)
+                        self.presentViewController(alertMessage, animated: true, completion: nil)
+                        
+                    }
+                })
+            }
+        }
+
+    
     }
     
-    func search(text:String) {
+    func filterContentForSearchText(searchText:String) {
         
 //        self.adsNearMe = []
 //        self.tableView.reloadData()
@@ -50,13 +94,13 @@ class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableVie
                 // Interested in locations near user.
                 query.whereKey(CLASSIFIED_AD.LOCATION, nearGeoPoint:self.myLocation)
                 query.whereKey(CLASSIFIED_AD.ACTIVE, equalTo: true)
-                NSLog("Searching for string \(text)")
-                if !text.isEmpty {
-                    let textArr = split(text.lowercaseString) {$0 == " "}
+                NSLog("Searching for string \(searchText)")
+                if !searchText.isEmpty {
+                    let textArr = split(searchText.lowercaseString) {$0 == " "}
                     query.whereKey(CLASSIFIED_AD.WORDS, containsAllObjectsInArray: textArr)
                 }
                 // Limit what could be a lot of points.
-                query.limit = 1000
+                query.limit = 100
                 // Final list of objects
 //                self.adsNearMe =
                 
@@ -68,9 +112,9 @@ class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableVie
                             
                             NSLog("Successfully retrieved \(objects.count)")
                             if objects.count > 0 {
-                                self.adsNearMe = objects as [PFObject]
+                                self.fileteredAdsNearMe = objects as [PFObject]
                                 self.searchDisplayController?.searchResultsTableView.reloadData()
-                                self.searchDisplayController?.searchResultsTableView.reloadInputViews()
+//                                self.searchDisplayController?.searchResultsTableView.reloadInputViews()
                             }
 
                         } else {
@@ -90,30 +134,43 @@ class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func searchBar(searchBar: UISearchBar,
         textDidChange searchText: String) {
-            search(searchBar.text)
-        println("editing")
+            filterContentForSearchText(searchBar.text)
+            println("editing")
     }
     
     
     func searchBarTextDidEndEditing(searchBar: UISearchBar) {
-        search(searchBar.text)
+        filterContentForSearchText(searchBar.text)
         println("searching")
     }
     
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.adsNearMe.count
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return self.fileteredAdsNearMe.count
+        } else {
+            return self.adsNearMe.count
+        }
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:AdSummaryTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("ad_summary") as AdSummaryTableViewCell
         
-        cell.summary.text = adsNearMe[indexPath.row][CLASSIFIED_AD.DESCRIPTION] as? String
-        cell.details.text = adsNearMe[indexPath.row][CLASSIFIED_AD.DESCRIPTION] as? String
+        var advertizement:PFObject
+        
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            advertizement = fileteredAdsNearMe[indexPath.row]
+        } else {
+            advertizement = adsNearMe[indexPath.row]
+        }
+        
+        cell.summary.text = advertizement[CLASSIFIED_AD.DESCRIPTION] as? String
+        cell.details.text = advertizement[CLASSIFIED_AD.DESCRIPTION] as? String
         
         let numberOfPlaces = 2.0
         let multiplier = pow(10.0, numberOfPlaces)
-        let distance = (adsNearMe[indexPath.row][CLASSIFIED_AD.LOCATION] as PFGeoPoint).distanceInMilesTo(myLocation)
+        let distance = (advertizement[CLASSIFIED_AD.LOCATION] as PFGeoPoint).distanceInMilesTo(myLocation)
         let roundedDistance = round(distance * multiplier) / multiplier
 
         cell.distance.text = "\(roundedDistance) Miles"
@@ -151,12 +208,12 @@ class SearchAdsViewController: UIViewController, UITableViewDelegate, UITableVie
     
     
     func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
-        self.search(searchString)
+        self.filterContentForSearchText(searchString)
         return true
     }
     
     func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
-        self.search(self.searchDisplayController!.searchBar.text)
+        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text)
         return true
     }
     
