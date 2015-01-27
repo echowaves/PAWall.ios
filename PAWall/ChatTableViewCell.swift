3362c975f2d31c34975f9c2ce4d6af4ce7b8ec3d
dmitryame <dmitry@custommode.com>
2015-01-26 21:47:58 -0500

    WIP

---
+++ PAWall/ChatTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChatTableViewCell.swift
+//  PAWall
+//
+//  Created by D on 1/26/15.
+//  Copyright (c) 2015 echowaves. All rights reserved.
+//
+
+import Foundation
+
+class ChatTableViewCell: UITableViewCell {
+    @IBOutlet weak var postedAt: UILabel!
+    @IBOutlet weak var body: UITextView!
+}
--- PAWall/ChatViewController.swift
+++ PAWall/ChatViewController.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var chatMessages = [PFObject]()
     var geoPostObject:PFObject?
     
     @IBOutlet weak var textView: UITextView!
@@ -23,12 +24,85 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("inside ChatViewController, geoPostObject: \(geoPostObject![GEO_POST.BODY])")
+        self.tableView.delegate      =   self
+        self.tableView.dataSource    =   self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // Create a query for places
+        var query = PFQuery(className:REPLY_POST.CLASS_NAME)
+        // Interested in locations near user.
         
+        query.whereKey(REPLY_POST.PARENT, equalTo: geoPostObject?.objectId)
+        query.orderByAscending("createdAt")
+        
+        // Limit what could be a lot of points.
+        
+        query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                // Do something with the found objects
+                
+                NSLog("Successfully retrieved \(objects.count)")
+                self.chatMessages = objects as [PFObject]
+                self.chatMessages.append(self.geoPostObject!)
+//                NSLog("there are \(self.chatMessages.count) chat messages")
+                self.tableView.reloadData()
+//                self.tableView.reloadInputViews()
+            } else {
+                // Log details of the failure
+                NSLog("Error: %@ %@", error, error.userInfo!)
+                
+                let alertMessage = UIAlertController(title: "Error", message: "Error retreiving ads, try agin.", preferredStyle: UIAlertControllerStyle.Alert)
+                let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in})
+                alertMessage.addAction(ok)
+                self.presentViewController(alertMessage, animated: true, completion: nil)
+                
+            }
+        })
     }
 
-    
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        NSLog("there are \(chatMessages.count) chat messages")
+        return self.chatMessages.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:ChatTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("chat_cell") as ChatTableViewCell
+        
+        var chatMessage:PFObject
+        
+        chatMessage = chatMessages[indexPath.row]
+        
+        let df = NSDateFormatter()
+        df.dateFormat = "MM-dd-yyyy"
+        cell.postedAt.text = NSString(format: "%@", df.stringFromDate(chatMessage.createdAt))
+        
+        if(chatMessage.parseClassName! == "GeoPosts") {
+            NSLog("rendering GeoPost")
+            cell.body.text = chatMessage[GEO_POST.BODY] as? String
+        } else {
+            NSLog("Rendering ReplyPost")
+            cell.body.text = chatMessage[REPLY_POST.BODY] as? String
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+
+    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        println("You selected cell #\(indexPath.row)!")
+//        self.performSegueWithIdentifier("myad_details", sender: self)
+//    }
+
     
 }
