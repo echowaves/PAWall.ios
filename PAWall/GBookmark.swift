f93f0b8039a6f37b5e0aa0b99a065b3298497727
dmitryame <dmitry@custommode.com>
2015-03-07 23:46:56 -0500

    formatting

--- PAWall/GBookmark.swift
+++ PAWall/GBookmark.swift
@@ -20,38 +20,37 @@ class GBookmark : BaseDataModel {
     
     
     class func createBookmark(myBookmark: String) -> Void {
-
-    var gBookmark:PFObject = PFObject(className:GBOOKMARK.CLASS_NAME)
-    var query:PFQuery = PFQuery(className:GBOOKMARK.CLASS_NAME)
-    query.whereKey(GBOOKMARK.SEARCH_TEXT, equalTo: myBookmark)
-    query.whereKey(GBOOKMARK.CREATED_BY, equalTo: DEVICE_UUID)
-    
-    query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
-    if error == nil {
-    // The find succeeded.
-    // Do something with the found objects
-    
-    NSLog("Successfully retrieved \(objects.count) alerts")
-    if objects.count > 0 {
-    // update the found alert here
-    gBookmark = objects[0] as PFObject
-    } else {
-    gBookmark[GBOOKMARK.SEARCH_TEXT] = myBookmark
-    gBookmark[GBOOKMARK.CREATED_BY] = DEVICE_UUID
-    }
-    gBookmark[GBOOKMARK.LOCATION] = APP_DELEGATE.getCurrentLocation()!
-    gBookmark.saveEventually()
-    
-    } else {
-    // Log details of the failure
-    NSLog("Error: %@ %@", error, error.userInfo!)
-    //                let alertMessage = UIAlertController(title: "Error", message: "Error retreiving alerts, try agin.", preferredStyle: UIAlertControllerStyle.Alert)
-    //                let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in})
-    //                alertMessage.addAction(ok)
-    //                self.presentViewController(alertMessage, animated: true, completion: nil)
-    
-    }
-    })
+        var gBookmark:PFObject = PFObject(className:GBOOKMARK.CLASS_NAME)
+        var query:PFQuery = PFQuery(className:GBOOKMARK.CLASS_NAME)
+        query.whereKey(GBOOKMARK.SEARCH_TEXT, equalTo: myBookmark)
+        query.whereKey(GBOOKMARK.CREATED_BY, equalTo: DEVICE_UUID)
+        
+        query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                // Do something with the found objects
+                
+                NSLog("Successfully retrieved \(objects.count) alerts")
+                if objects.count > 0 {
+                    // update the found alert here
+                    gBookmark = objects[0] as PFObject
+                } else {
+                    gBookmark[GBOOKMARK.SEARCH_TEXT] = myBookmark
+                    gBookmark[GBOOKMARK.CREATED_BY] = DEVICE_UUID
+                }
+                gBookmark[GBOOKMARK.LOCATION] = APP_DELEGATE.getCurrentLocation()!
+                gBookmark.saveEventually()
+                
+            } else {
+                // Log details of the failure
+                NSLog("Error: %@ %@", error, error.userInfo!)
+                //                let alertMessage = UIAlertController(title: "Error", message: "Error retreiving alerts, try agin.", preferredStyle: UIAlertControllerStyle.Alert)
+                //                let ok = UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in})
+                //                alertMessage.addAction(ok)
+                //                self.presentViewController(alertMessage, animated: true, completion: nil)
+                
+            }
+        })
     }
     
 }
